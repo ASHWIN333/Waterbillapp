350de1f192fa467eea9621882c21dde3c5d04cb5
ASHWIN333 <62065071+ASHWIN333@users.noreply.github.com>
2021-05-06 17:24:48 +0530

    Add files via upload

---
+++ PerUnitPrice.java
@@ -0,0 +1,14 @@
+package in.waterbillingcalculator;
+
+public class PerUnitPrice 
+{
+	public int minimumUnits;
+	
+	public int maximumUnits;
+	
+	public String type;
+	
+	public int price;
+	
+	
+}
